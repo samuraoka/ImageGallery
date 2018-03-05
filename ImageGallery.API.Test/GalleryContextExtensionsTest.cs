1cd9071c4fd977c4e14b0367b495ce05dff17e71
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-05 19:58:32 +0900

    Implemented GalleryContextExtensions class.

---
+++ ImageGallery.API.Test/GalleryContextExtensionsTest.cs
@@ -0,0 +1,45 @@
+﻿using ImageGallery.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ImageGallery.API.Test
+{
+    public class GalleryContextExtensionsTest
+    {
+        private readonly DbContextOptions<GalleryContext> options;
+
+        public GalleryContextExtensionsTest()
+        {
+            options = new DbContextOptionsBuilder<GalleryContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using (var ctx = new GalleryContext(options))
+            {
+                ctx.Database.EnsureDeleted();
+                ctx.Database.EnsureCreated();
+            }
+        }
+
+        [Fact]
+        public void CouldAddSeedData()
+        {
+            // Act
+            using (var ctx = new GalleryContext(options))
+            {
+                ctx.EnsureSeedDataForContext();
+            }
+
+            // Assert
+            List<Image> images = null;
+            using (var ctx = new GalleryContext(options))
+            {
+                images = ctx.Images.ToList();
+            }
+            Assert.Equal(14, images.Count);
+        }
+    }
+}
