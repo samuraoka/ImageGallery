3402b949934c73dcb06cc295e0dffa3e88151d22
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-05 15:23:49 +0900

    Added ImageForUpdate class.

---
+++ ImageGallery.Model.Test/ImageForUpdateTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace ImageGallery.Model.Test
+{
+    public class ImageForUpdateTest
+    {
+        [Fact]
+        public void ShouldCreateNewInstance()
+        {
+            // Act
+            var obj = new ImageForUpdate();
+
+            // Assert
+            Assert.NotNull(obj);
+            Assert.True(obj is ImageForUpdate);
+        }
+    }
+}
