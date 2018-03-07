42f8986e003b0cb5d4fe1d56367d6ed3a7caf46d
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-07 23:50:04 +0900

    Created Index action method for the BalleryController class.

---
+++ ImageGallery.Client.Test/GalleryControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ImageGallery.Client.Test
+{
+    public class GalleryControllerTest
+    {
+        public ITestOutputHelper Output { get; }
+
+        public GalleryControllerTest(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        [Fact]
+        public void ShouldGetExceptionWhenIndexActionErrors()
+        {
+            // Arrange
+            var controller = new GalleryController();
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => controller.Index());
+            Output.WriteLine($"exception message: {exception.Message}");
+            Assert.StartsWith("A problem happend while calling the API:", exception.Message);
+        }
+    }
+}
---
+++ ImageGallery.Client/Controllers/GalleryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ImageGallery.Client
+{
+    public class GalleryController
+    {
+        public GalleryController()
+        {
+        }
+
+        public void Index()
+        {
+            // TODO implemente index process
+
+            var response = new HttpResponseMessage();
+
+            // TODO implemente index process
+
+            throw new Exception($"A problem happend while calling the API: {response.ReasonPhrase}");
+        }
+    }
+}
