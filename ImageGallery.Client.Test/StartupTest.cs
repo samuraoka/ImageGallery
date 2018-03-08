a012af7a6d48305507f66cd37b6f1471b432bb26
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-09 02:57:17 +0900

    Added some services.
    changed default map route.

---
+++ ImageGallery.Client.Test/StartupTest.cs
@@ -0,0 +1,70 @@
+﻿using ImageGallery.Client.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using Xunit;
+
+namespace ImageGallery.Client.Test
+{
+    public class StartupTest : IDisposable
+    {
+        public TestServer Server { get; private set; }
+
+        public StartupTest()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (Server != null)
+                {
+                    Server.Dispose();
+                    Server = null;
+                }
+            }
+        }
+
+        [Fact]
+        public void ShouldGetHttpContextAccessorService()
+        {
+            // Act
+            var service = Server.Host.Services.GetService(typeof(IHttpContextAccessor));
+
+            // Assert
+            Assert.NotNull(service);
+            Assert.IsAssignableFrom<IHttpContextAccessor>(service);
+        }
+
+        [Fact]
+        public void ShouldGetHttpContextAccessorServiceAsSingleton()
+        {
+            // Act
+            var service1 = Server.Host.Services.GetService(typeof(IHttpContextAccessor));
+            var service2 = Server.Host.Services.GetService(typeof(IHttpContextAccessor));
+
+            // Assert
+            Assert.Same(service1, service2);
+        }
+
+        [Fact]
+        public void ShouldGetImageGalleryHttpClientService()
+        {
+            // Act
+            var service = Server.Host.Services.GetService(typeof(IImageGalleryHttpClient));
+
+            // Assert
+            Assert.NotNull(service);
+            Assert.IsAssignableFrom<IImageGalleryHttpClient>(service);
+        }
+    }
+}
--- ImageGallery.Client/Startup.cs
+++ ImageGallery.Client/Startup.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ImageGallery.Client.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,12 +19,21 @@ namespace ImageGallery.Client
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
             services.AddMvc();
+
+            // register an IHttpContextAccessor so we can access the current
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // register an IImageGalleryHttpClient
+            services.AddScoped<IImageGalleryHttpClient, ImageGalleryHttpClient>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //TODO Add logging setting if necessary.
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -43,7 +50,7 @@ namespace ImageGallery.Client
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Gallery}/{action=Index}/{id?}");
             });
         }
     }
