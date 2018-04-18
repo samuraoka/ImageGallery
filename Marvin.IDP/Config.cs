95dac3bea25025d4d7c10c9ce50ff85a25c272d3
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-04-18 09:37:44 +0900

    Implemented some code
    Demo - Setting up IdentityServer4
    Demo - Adding a User Interface for IdentityServer4

---
+++ Marvin.IDP/Config.cs
@@ -0,0 +1,69 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        // Adding users
+        // http://docs.identityserver.io/en/release/quickstarts/2_resource_owner_passwords.html#adding-users
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                    Username = "Frank",
+                    Password = "password",
+
+                    // Further experiments
+                    // http://docs.identityserver.io/en/release/quickstarts/3_interactive_login.html?highlight=claim#further-experiments
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwood"),
+                    },
+                },
+                new TestUser
+                {
+                    SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                    Username = "Claire",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Claire"),
+                        new Claim("family_name", "Underwood"),
+                    },
+                },
+            };
+        }
+
+        // Adding support for OpenID Connect Identity Scopes
+        // http://docs.identityserver.io/en/release/quickstarts/3_interactive_login.html#adding-support-for-openid-connect-identity-scopes
+        //
+        // 5.4.  Requesting Claims using Scope Values
+        // https://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        // Defining the client
+        // http://docs.identityserver.io/en/release/quickstarts/1_client_credentials.html#defining-the-client
+        //
+        // Modifying the client configuration
+        // http://docs.identityserver.io/en/release/quickstarts/5_hybrid_and_api_access.html#modifying-the-client-configuration
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+    }
+}
--- Marvin.IDP/Startup.cs
+++ Marvin.IDP/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Marvin.IDP
@@ -15,6 +10,24 @@ namespace Marvin.IDP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Introduction to Razor Pages in ASP.NET Core
+            // https://docs.microsoft.com/en-us/aspnet/core/mvc/razor-pages/?view=aspnetcore-2.0&tabs=visual-studio
+            services.AddMvc();
+
+            // IdentityServer4
+            // https://www.nuget.org/packages/IdentityServer4/
+            // Install-Package -Id IdentityServer4 -ProjectName Marvin.IDP
+            //
+            // Setup and Overview
+            // http://docs.identityserver.io/en/release/quickstarts/0_overview.html#setup-and-overview
+            //
+            // Configure IdentityServer
+            // http://docs.identityserver.io/en/release/quickstarts/1_client_credentials.html#configure-identityserver
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryClients(Config.GetClients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +38,17 @@ namespace Marvin.IDP
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // Work with static files in ASP.NET Core
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/static-files?view=aspnetcore-2.0&tabs=aspnetcore2x
+            app.UseStaticFiles();
+
+            // Basic setup
+            // http://docs.identityserver.io/en/release/quickstarts/0_overview.html#basic-setup
+            app.UseIdentityServer();
+
+            // Introduction to Razor Pages in ASP.NET Core
+            // https://docs.microsoft.com/en-us/aspnet/core/mvc/razor-pages/?view=aspnetcore-2.0&tabs=visual-studio
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
